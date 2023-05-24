548782080ba104dadd6e4eb8fd98cc9ad2bb3cef
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-24 17:10:34 +0400

    -------------------

---
+++ src/main/java/com/alialsubhi/SchoolManagementSystem/Models/BaseEntity.java
@@ -0,0 +1,12 @@
+package com.alialsubhi.SchoolManagementSystem.Models;
+
+import java.util.Date;
+
+public class BaseEntity {
+
+    private Date createdDate;
+
+    private boolean isActive;
+    private Date updatedDate;
+
+}
